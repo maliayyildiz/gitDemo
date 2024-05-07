d89864c7bee13a6b80c9de40ceed3a63abc073b8
aysebetty <95138426+aysebetty@users.noreply.github.com>
2024-05-07 17:44:26 -0400

    main method ekledim

--- src/Class02.java
+++ src/Class02.java
@@ -2,4 +2,15 @@ public class Class02 {
     //sadece a ve b degiskenini kullanarak degerlerin yerlerinidegistir
     //orn:  a= 5 b=3
     //sonc a=3 b =5
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
 }
