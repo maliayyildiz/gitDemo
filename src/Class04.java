3a1fa0dc1da349fda1dede7beb43c11319af1e70
Humaipek <153774907+Humaipek@users.noreply.github.com>
2024-05-08 00:16:50 +0200

    task

--- src/Class04.java
+++ src/Class04.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class Class04 {
     //kullanicinin adini soy adni memeleketini ayri ayri al ve tek satirda yazdir.
     //memeleket buyuk harfler yazilsin.
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        String name=scanner.next();
+        String lastName=scanner.next();
+        String city=scanner.next().toUpperCase();
+        System.out.println(name+" "+lastName+" ; "+city);
+
+    }
+
+
 }
