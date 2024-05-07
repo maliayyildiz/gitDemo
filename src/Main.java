dcb177f58d3f469a4d7b60ea00b27eec33feb9da
MurvetAyyildiz <murvetayyildiz@gmail.com>
2024-05-07 23:43:54 +0200

    txt file ekledim

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        //sadece a ve b degiskenini kullanarak degerlerin yerlerinidegistir
+        //orn:  a= 5 b=3
+        //sonc a=3 b =5
+        int a = 5;
+        int b = 3;
+        int c=0;
+
+        c=b;
+        b=a;
+        a=c;
+
+
+
+
     }
 }
