f4b37675a07148ea8204674055d76df2de9e4238
Orhun Gunes ABCAN <gunes.abcan@gmail.com>
2024-05-08 00:44:09 +0300

    Class01OrhunGunesABCAN

--- src/Class01.java
+++ src/Class01.java
@@ -1,4 +1,60 @@
+import java.util.Scanner;
+
 public class Class01 {
-    //hangi ayda oldugumuzu sor ve o yin rakamsal degerini yazdir
-    //or Ocak - > 01
+    public static void main(String[] args) {
+        //hangi ayda oldugumuzu sor ve o yin rakamsal degerini yazdir
+        //or Ocak - > 01
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hangi aydayız? : ");
+        String ay = scanner.nextLine();
+
+        ay = ay.toLowerCase();
+
+        int rakamDegeri = 0;
+
+        switch (ay) {
+            case "ocak":
+                rakamDegeri = 1;
+                break;
+            case "şubat":
+                rakamDegeri = 2;
+                break;
+            case "mart":
+                rakamDegeri = 3;
+                break;
+            case "nisan":
+                rakamDegeri = 4;
+                break;
+            case "mayıs":
+                rakamDegeri = 5;
+                break;
+            case "haziran":
+                rakamDegeri = 6;
+                break;
+            case "temmuz":
+                rakamDegeri = 7;
+                break;
+            case "ağustos":
+                rakamDegeri = 8;
+                break;
+            case "eylül":
+                rakamDegeri = 9;
+                break;
+            case "ekim":
+                rakamDegeri = 10;
+                break;
+            case "kasım":
+                rakamDegeri = 11;
+                break;
+            case "aralık":
+                rakamDegeri = 12;
+                break;
+            default:
+                System.out.println("Geçersiz ay ismi girdiniz!");
+        }
+
+        System.out.println("Girdiğiniz ayın rakamsal değeri: " + rakamDegeri);
+
+    }
+
 }
