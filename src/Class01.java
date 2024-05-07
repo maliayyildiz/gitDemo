8751965d74a2420cabdc12ecceb557c1e7137b22
Mukaddes KUCUK <mukaddeszorlu@gmail.com>
2024-05-08 00:29:05 +0200

    07-05-2024

--- src/Class01.java
+++ src/Class01.java
@@ -1,4 +1,27 @@
-public class Class01 {
-    //hangi ayda oldugumuzu sor ve o yin rakamsal degerini yazdir
-    //or Ocak - > 01
-}
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+    //hangi ayda oldugumuzu sor ve o yilin rakamsal degerini yazdir
+    //    //or Ocak - > 01
+
+    public class Class01 {
+        public static void main(String[] args) {
+
+            LocalDate tarih = LocalDate.now();
+
+            int aySayisal = tarih.getMonthValue();
+
+            String ayIsmi = tarih.getMonth().name();
+
+            // Sayısal değeri iki basamaklı formatta yazdır
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+            String aySayisalFormatli = tarih.format(formatter);
+
+            System.out.println("Bugünün ayı: " + ayIsmi);
+            System.out.println("Ayın sayısal değeri: " + aySayisalFormatli);
+        }
+    }
+
+
+
