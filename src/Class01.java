63cdfba0aae5f47451bca5429f0132439b2d76b3
Emine Aker <portakal5516@gmail.com>
2024-05-08 00:34:53 +0200

    java

--- src/Class01.java
+++ src/Class01.java
@@ -1,4 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Class01 {
+
     //hangi ayda oldugumuzu sor ve o yin rakamsal degerini yazdir
     //or Ocak - > 01
+
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        String monthNumber = DateTimeFormatter.ofPattern("MM").format(currentDate);
+
+        System.out.println("Mevcut ay: " + currentDate.getMonth());
+        System.out.println("Mevcut ayın rakamsal değeri: " + monthNumber);
+    }
+
+
 }
