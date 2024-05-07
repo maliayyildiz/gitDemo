e3e0046a44bb85fef36964db8543a32b3a8b8d7c
Mukaddes KUCUK <mukaddeszorlu@gmail.com>
2024-05-08 01:30:55 +0200

    versiyon

--- src/Class01.java
+++ src/Class01.java
@@ -1,30 +1,62 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 
-    //hangi ayda oldugumuzu sor ve o yilin rakamsal degerini yazdir
+//hangi ayda oldugumuzu sor ve o yilin rakamsal degerini yazdir
     //    //or Ocak - > 01
 
     public class Class01 {
         public static void main(String[] args) {
 
-            LocalDate tarih = LocalDate.now();
 
-            int aySayisal = tarih.getMonthValue();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Başlangic ay  numarası giriniz");
+            int aySayisi = input.nextInt();
+            switch (aySayisi) {
+                case 1:
+                    System.out.println("Ocak");
+                    break;
+                case 2:
+                    System.out.println("Subat");
+                    break;
+                case 3:
+                    System.out.println("Mart");
+                    break;
+                case 4:
 
-            String ayIsmi = tarih.getMonth().name();
+                    System.out.println("Nisan");
+                    break;
+                case 5:
+                    System.out.println("Mayis");
+                    break;
+                case 6:
+                    System.out.println("Haziran");
+                    break;
+                case 7:
+                    System.out.println("Temmuz");
+                    break;
+                case 8:
+                    System.out.println("Agustos");
+                    break;
+                case 9:
+                    System.out.println("eylul");
+                    break;
+                case 10:
+                    System.out.println("Ekim");
+                    break;
+                case 11:
+                    System.out.println("Kasım");
+                    break;
+                case 12:
+                    System.out.println("Aralık");
+                    break;
+                default:
+                    System.out.println("Gecerli bir ay numarası girmediniz");
 
-            // Sayısal değeri iki basamaklı formatta yazdır
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
-            String aySayisalFormatli = tarih.format(formatter);
 
-            System.out.println("Bugünün ayı: " + ayIsmi);
-            System.out.println("Ayın sayısal değeri: " + aySayisalFormatli);
-            System.out.println("............................");
+            }
 
 
         }
     }
-
-
-
--- src/Class02.java
+++ src/Class02.java
@@ -2,4 +2,8 @@ public class Class02 {
     //sadece a ve b degiskenini kullanarak degerlerin yerlerinidegistir
     //orn:  a= 5 b=3
     //sonc a=3 b =5
+
+
+
+
 }
